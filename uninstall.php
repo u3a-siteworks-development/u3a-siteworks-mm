e2485a0e3603c90f28ba4e768a780699aa8b65a7
Ed Slattery <se33159@gmail.com>
2023-11-02 10:55:24 +0000

    initial commit

---
+++ uninstall.php
@@ -0,0 +1,12 @@
+<?php
+
+// exit if uninstall constant is not defined
+
+if (!defined('WP_UNINSTALL_PLUGIN')) exit;
+
+// Remove the option fields used by the maintenance mode plugin
+
+delete_option('u3a_maintenance_active');
+delete_option('u3a_maintenance_reminder');
+delete_option('u3a_maintenance_msg');
+delete_option('u3a_maintenance_image');
